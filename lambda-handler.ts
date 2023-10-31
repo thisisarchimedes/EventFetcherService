3c78cb40b75cfbcb0bae90de01a3bc28bc6d921e
danielj86 <danieljafe@gmail.com>
2023-10-31 23:37:49 +0200

    move lambda-handler location

--- lambda-handler.ts
+++ lambda-handler.ts
@@ -1,4 +1,4 @@
-import { EventProcessorService } from './EventScanner' // Adjust the import to your file structure
+import { EventProcessorService } from './src/EventScanner' // Adjust the import to your file structure
 
 export const handler = async (event: any, context: any): Promise<void> => {
   const eventProcessorService = new EventProcessorService()
