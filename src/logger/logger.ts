57aaa3ea25a2f99e17bc1b95a0ca55d4df7baf3f
danielj86 <danieljafe@gmail.com>
2023-11-02 17:44:36 +0200

    Update logger.ts

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,30 +1,46 @@
-import newrelic from 'newrelic';
-
 export class Logger {
-    // Function to log information
     info(message: string, metaData?: any): void {
-        console.info(message);
-        if (metaData) {
-            newrelic.addCustomAttribute('info', metaData);
-        }
-        newrelic.recordCustomEvent('InfoEvent', { message });
+        console.info(message, metaData ? metaData : '');
     }
 
     // Function to log warnings
     warn(message: string, metaData?: any): void {
-        console.warn(message);
-        if (metaData) {
-            newrelic.addCustomAttribute('warn', metaData);
-        }
-        newrelic.recordCustomEvent('WarnEvent', { message });
+        console.warn(message, metaData ? metaData : '');
     }
 
     // Function to log errors
     error(message: string, error: Error, metaData?: any): void {
-        console.error(message, error);
-        newrelic.noticeError(error, { message });
-        if (metaData) {
-            newrelic.addCustomAttribute('error', metaData);
-        }
+        console.error(message, error, metaData ? metaData : '');
     }
 }
+
+// import newrelic from 'newrelic';
+
+// export class Logger {
+//     // Function to log information
+//     info(message: string, metaData?: any): void {
+//         console.info(message);
+//         if (metaData) {
+//             newrelic.addCustomAttribute('info', metaData);
+//         }
+//         newrelic.recordCustomEvent('InfoEvent', { message });
+//     }
+
+//     // Function to log warnings
+//     warn(message: string, metaData?: any): void {
+//         console.warn(message);
+//         if (metaData) {
+//             newrelic.addCustomAttribute('warn', metaData);
+//         }
+//         newrelic.recordCustomEvent('WarnEvent', { message });
+//     }
+
+//     // Function to log errors
+//     error(message: string, error: Error, metaData?: any): void {
+//         console.error(message, error);
+//         newrelic.noticeError(error, { message });
+//         if (metaData) {
+//             newrelic.addCustomAttribute('error', metaData);
+//         }
+//     }
+// }
