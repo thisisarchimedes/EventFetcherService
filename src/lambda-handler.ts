9592fffdaab496502f9c9ac08e4ef4f4da2953b8
danielj86 <danieljafe@gmail.com>
2023-11-05 15:47:51 +0200

    new relic

--- src/lambda-handler.ts
+++ src/lambda-handler.ts
@@ -1,7 +1,7 @@
-// require('newrelic');
+require('newrelic');
 import { EventProcessorService } from './EventScanner';
 
 export const handler = async (event: any, context: any): Promise<void> => {
-    const eventProcessorService = new EventProcessorService();
-    await eventProcessorService.execute();
+  const eventProcessorService = new EventProcessorService();
+  await eventProcessorService.execute();
 };
