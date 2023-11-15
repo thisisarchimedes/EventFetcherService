7e9a586e9d03672634d50a4bd582babe60a8eac9
danielj86 <danieljafe@gmail.com>
2023-11-15 13:01:10 +0200

    try winston

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,46 +1,29 @@
-// export class Logger {
-//   info(message: string, metaData?: any): void {
-//     console.info(message, metaData ? metaData : '');
-//   }
-
-//   // Function to log warnings
-//   warn(message: string, metaData?: any): void {
-//     console.warn(message, metaData ? metaData : '');
-//   }
-
-//   // Function to log errors
-//   error(message: string, error: Error, metaData?: any): void {
-//     console.error(message, error, metaData ? metaData : '');
-//   }
-// }
-
-import newrelic from 'newrelic';
+import { createLogger } from 'winston';
+import WinstonNewrelicLogsTransport from 'winston-newrelic-logs-transport';
+const logger = createLogger({
+  transports: [
+    new WinstonNewrelicLogsTransport({
+      licenseKey: process.env.NEW_RELIC_LICENSE_KEY ?? '',
+      apiUrl: process.env.NEW_RELIC_API_URL ?? '',
+    }),
+  ],
+});
 
 export class Logger {
-  // Function to log information
   info(message: string, metaData?: any): void {
-    console.info(message);
-    if (metaData) {
-      newrelic.addCustomAttribute('info', metaData);
-    }
-    newrelic.recordCustomEvent('InfoEvent', { message });
+    logger.info(message, metaData ? metaData : '');
+    console.info(message, metaData ? metaData : '');
   }
 
   // Function to log warnings
   warn(message: string, metaData?: any): void {
-    console.warn(message);
-    if (metaData) {
-      newrelic.addCustomAttribute('warn', metaData);
-    }
-    newrelic.recordCustomEvent('WarnEvent', { message });
+    logger.warn(message, metaData ? metaData : '');
+    console.warn(message, metaData ? metaData : '');
   }
 
   // Function to log errors
   error(message: string, error: Error, metaData?: any): void {
-    console.error(message, error);
-    newrelic.noticeError(error, { message });
-    if (metaData) {
-      newrelic.addCustomAttribute('error', metaData);
-    }
+    logger.error(message, error, metaData ? metaData : '');
+    console.error(message, error, metaData ? metaData : '');
   }
 }
