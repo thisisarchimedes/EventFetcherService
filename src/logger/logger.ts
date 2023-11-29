177717c18957562fbf1c3d143de6aa6ba07a683a
danielj86 <danieljafe@gmail.com>
2023-11-29 18:03:08 +0200

    disable new relic

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -11,19 +11,19 @@ const logger = createLogger({
 
 export class Logger {
   info(message: string, metaData?: any): void {
-    logger.info(message, metaData ? metaData : '');
+    // logger.info(message, metaData ? metaData : '');
     console.info(message, metaData ? metaData : '');
   }
 
   // Function to log warnings
   warn(message: string, metaData?: any): void {
-    logger.warn(message, metaData ? metaData : '');
+    // logger.warn(message, metaData ? metaData : '');
     console.warn(message, metaData ? metaData : '');
   }
 
   // Function to log errors
   error(message: string, error: Error, metaData?: any): void {
-    logger.error(message, error, metaData ? metaData : '');
+    // logger.error(message, error, metaData ? metaData : '');
     console.error(message, error, metaData ? metaData : '');
   }
 }
