871d7dfee0ac1fffc9759d4c59d431ae333bbc5f
danielj86 <danieljafe@gmail.com>
2023-11-30 14:02:01 +0200

    move initialization outside handler function

--- src/lambda-handler.ts
+++ src/lambda-handler.ts
@@ -5,18 +5,26 @@ import { SQSService } from './services/sqsService';
 import { ethers } from 'ethers';
 import { ConfigService } from './services/configService';
 
-export const handler = async (event: any, context: any): Promise<void> => {
-  const alchemyProvider = new ethers.providers.JsonRpcProvider(
-    process.env.ALCHEMY_API_URL ?? '',
-  );
-  const infuraProvider = new ethers.providers.JsonRpcProvider(
-    process.env.INFURA_API_URL ?? '',
-  );
-  const s3Service = new S3Service();
-  const sqsService = new SQSService();
-  const logger = new Logger();
+// Moved outside the handler function
+const alchemyProvider = new ethers.providers.JsonRpcProvider(
+  process.env.ALCHEMY_API_URL ?? '',
+);
+const infuraProvider = new ethers.providers.JsonRpcProvider(
+  process.env.INFURA_API_URL ?? '',
+);
+const s3Service = new S3Service();
+const sqsService = new SQSService();
+const logger = new Logger();
+let leverageEngineAddress: string;
 
-  const leverageEngineAddress = await new ConfigService().getLeverageEngineAddress();
+const initializeDependencies = async () => {
+  if (!leverageEngineAddress) {
+    leverageEngineAddress = await new ConfigService().getLeverageEngineAddress();
+  }
+};
+
+export const handler = async (event: any, context: any): Promise<void> => {
+  await initializeDependencies();
 
   const eventProcessorService = new EventProcessorService(
     alchemyProvider,
