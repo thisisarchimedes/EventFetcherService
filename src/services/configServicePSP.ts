e6ab011e6254fcea490d5d29ca9430df898ae0aa
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-07 16:44:33 -0500

    Update configServicePSP.ts

--- src/services/configServicePSP.ts
+++ src/services/configServicePSP.ts
@@ -1,45 +1,55 @@
-import {S3Service} from '@thisisarchimedes/backend-sdk';
+import { S3Service } from '@thisisarchimedes/backend-sdk';
 
-export type PSPStrategyConfig = {
-    strategyName: string;
-    strategyAddress: string; 
-};
+export interface PSPStrategyConfig {
+  strategyName: string;
+  strategyAddress: string;
+}
 
 export class ConfigServicePSP {
-  private strategies: PSPStrategyConfig[];
-  private readonly s3: S3Service;
+  private strategies: PSPStrategyConfig[] = [];
+  private readonly s3Service: S3Service;
   private readonly bucketName: string;
   private readonly fileName: string;
 
   constructor(bucketName: string, fileName: string) {
-    this.s3 = new S3Service();
+    this.s3Service = new S3Service();
     this.bucketName = bucketName;
     this.fileName = fileName;
-    this.strategies = []; 
   }
 
-  async refreshStrategyConfig() {
+  public async refreshStrategyConfig(): Promise<void> {
     try {
-      const tmp = await this.s3.getObject(this.bucketName, this.fileName);
-      const jsonObj = JSON.parse(tmp.toString()); 
-
-      this.strategies = [];
-
-      jsonObj.forEach((item: any) => {
-        this.strategies.push({
-          strategyName: item.strategyName,
-          strategyAddress: item.strategyAddress, 
-        });
-      });
+      const objectData = await this.fetchS3Object(this.bucketName, this.fileName);
+      const strategies = this.parseStrategyConfigs(objectData);
+      this.updateStrategies(strategies);
     } catch (error) {
+      // TODO: Log line
       console.error('Error refreshing strategy config:', error);
     }
   }
 
-  getStrategyConfigByIndex(index: number): PSPStrategyConfig | undefined {
-    if (index >= 0 && index < this.strategies.length) {
-      return this.strategies[index];
+  public getStrategyConfigByIndex(index: number): PSPStrategyConfig | undefined {
+    return index >= 0 && index < this.strategies.length ? this.strategies[index] : undefined;
+  }
+
+  private async fetchS3Object(bucket: string, file: string): Promise<string> {
+    const response = await this.s3Service.getObject(bucket, file);
+    return response.toString();
+  }
+
+  private parseStrategyConfigs(data: string): PSPStrategyConfig[] {
+    try {
+      const parsedData: any[] = JSON.parse(data);
+      return parsedData.map(item => ({
+        strategyName: item.strategyName,
+        strategyAddress: item.strategyAddress,
+      }));
+    } catch (error) {
+      throw new Error('Failed to parse strategy configurations');
     }
-    return undefined;
+  }
+
+  private updateStrategies(strategies: PSPStrategyConfig[]): void {
+    this.strategies = strategies;
   }
 }
