27207b429974234a08d421b1ad199b2bc9fe982b
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-15 12:33:15 -0500

    Update ConfigServicePSP.ts

--- src/services/config/ConfigServicePSP.ts
+++ src/services/config/ConfigServicePSP.ts
@@ -26,10 +26,12 @@ export class ConfigServicePSP extends ConfigService {
       const strategies = this.parseStrategyConfigs(objectData);
       console.log('>> 3 - refreshStrategyConfig, strategies: ', strategies);
       this.updateStrategies(strategies);
+      console.log('>> 4 - refreshStrategyConfig, this.strategies: ', this.strategies);
     } catch (error) {
       // TODO: Log line
       console.error('Error refreshing strategy config:', error);
     }
+    console.log('>> 5 - refreshStrategyConfig');
   }
 
   public getStrategyConfigByIndex(index: number): IPSPStrategyConfig | undefined {
