6795397b3803fbe5d28399bb076d9407f2924640
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-15 16:19:15 -0500

    Update ConfigService.test.ts

--- test/unit/ConfigService.test.ts
+++ test/unit/ConfigService.test.ts
@@ -7,38 +7,65 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 describe('Config Service Test', function() {
+  let awsS3Client: AWS.S3;
+  let configService: ConfigServiceLocal;
+
+  beforeEach(async function() {
+    awsS3Client = createAwsS3Client();
+    configService = new ConfigServiceLocal('demo');
+    await configService.refreshConfig();
+  });
+
   it('should get the correct leverage contract addresses when run locally', async function() {
-    const s3 = new AWS.S3({
+    const leverageContractAddresses = await fetchLeverageContractAddresses();
+
+    validateContractAddress(
+        leverageContractAddresses,
+        'PositionOpener',
+        configService.getLeveragePositionOpenerAddress(),
+    );
+    validateContractAddress(
+        leverageContractAddresses,
+        'PositionLiquidator',
+        configService.getLeveragePositionLiquidatorAddress(),
+    );
+    validateContractAddress(
+        leverageContractAddresses,
+        'PositionCloser',
+        configService.getLeveragePositionCloserAddress(),
+    );
+  });
+
+  function validateContractAddress(
+      addresses: ContractAddress[],
+      contractName: string,
+      actualAddress: string,
+  ) {
+    const expectedAddress = addresses.find((contract) => contract.name === contractName)?.address;
+    expect(actualAddress).to.equal(expectedAddress);
+  }
+
+  function createAwsS3Client() {
+    return new AWS.S3({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: process.env.AWS_REGION,
     });
+  }
 
-    const params = {
-      Bucket: process.env.S3_BUCKET_CONFIG || '',
-      Key: process.env.S3_DEPLOYMENT_ADDRESS_KEY || '',
-    };
-    if (!params.Bucket || !params.Key) {
-      throw new Error('S3 bucket or key is undefined.');
-    }
-    const data = await s3.getObject(params).promise();
-    const expectedConfig = data.Body.toString();
-    const leverageContractAddresses: ContractAddress[] = JSON.parse(expectedConfig);
+  async function fetchLeverageContractAddresses(): Promise<ContractAddress[]> {
+    const params = getS3Params();
+    const data = await awsS3Client.getObject(params).promise();
+    const configData = data.Body.toString();
+    return JSON.parse(configData);
+  }
 
-
-    const configService = new ConfigServiceLocal('demo');
-    await configService.refreshConfig();
-
-    const positionOpenerAddress = leverageContractAddresses.find((contract) => contract.name === 'PositionOpener');
-    expect(configService.getLeveragePositionOpenerAddress()).to.equal(positionOpenerAddress?.address);
-
-    const positionLiquidatorAddress = leverageContractAddresses.find((contract) =>
-      contract.name === 'PositionLiquidator');
-    expect(configService.getLeveragePositionLiquidatorAddress()).to.equal(positionLiquidatorAddress?.address);
-
-    const positionCloserAddress = leverageContractAddresses.find((contract) => contract.name === 'PositionCloser');
-    expect(configService.getLeveragePositionCloserAddress()).to.equal(positionCloserAddress?.address);
-  });
+  function getS3Params() {
+    const bucket = process.env.S3_BUCKET_CONFIG || '';
+    const key = process.env.S3_DEPLOYMENT_ADDRESS_KEY || '';
+    if (!bucket || !key) throw new Error('S3 bucket or key is undefined.');
+    return {Bucket: bucket, Key: key};
+  }
 });
 
 /*
