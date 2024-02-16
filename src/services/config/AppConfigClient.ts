6ccc2fe95dc3f010d7d87d7c42a5c5ad7e57eb9a
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-16 16:06:53 -0500

    update

---
+++ src/services/config/AppConfigClient.ts
@@ -0,0 +1,39 @@
+import {AppConfig, GetConfigurationCommandOutput} from '@aws-sdk/client-appconfig';
+
+export class AppConfigClient {
+  private readonly appConfigClient: AppConfig;
+  private readonly environment: string;
+  private readonly region: string;
+
+  constructor(environment: string, region: string) {
+    this.appConfigClient = new AppConfig({region: region});
+    this.region = region;
+    this.environment = environment;
+  }
+
+  public async fetchConfig(configName: string): Promise<string> {
+    try {
+      const response = await this.getConfigFromAppConfig(configName);
+      return response;
+    } catch (error) {
+      throw new Error(`Error fetching configuration for ${configName}: ${error}`);
+    }
+  }
+
+  private async getConfigFromAppConfig(configName: string): Promise<string> {
+    const params = {
+      Application: this.environment,
+      Configuration: configName,
+      Environment: this.environment,
+      ClientId: '1', // currently we are not using this feature
+    };
+
+    const response: GetConfigurationCommandOutput = await this.appConfigClient.getConfiguration(params);
+
+    if (response.Content) {
+      return Buffer.from(response.Content).toString('utf-8');
+    } else {
+      throw new Error(`Configuration ${configName} not found or is empty.`);
+    }
+  }
+}
--- test/interface/AWSAppConfig.test.ts
+++ test/interface/AWSAppConfig.test.ts
@@ -1,50 +1,20 @@
 import {expect} from 'chai';
-import {AppConfig} from '@aws-sdk/client-appconfig';
 import {ContractInfoPSP} from '../../src/types/ContractInfoPSP';
+import {AppConfigClient} from '../../src/services/config/AppConfigClient';
 
-const CONFIG = {
-  appName: 'DemoEnvironment',
-  configName: 'PSPStrategyInfo',
-  environmentName: 'Demo',
-  clientId: '1',
-  region: 'us-east-1',
-};
 
 describe('Config Service - Demo Environment', function() {
+  let appConfigClient: AppConfigClient;
+
+  beforeEach(function() {
+    appConfigClient = new AppConfigClient('Demo', 'us-east-1');
+  });
+
   it('should fetch PSP strategies via AWS AppConfig', async function() {
-    const strategies = await fetchPSPStrategies(CONFIG);
+    const strategies = JSON.parse(await appConfigClient.fetchConfig('PSPStrategyInfo')) as ContractInfoPSP[];
     expect(strategies).to.not.be.undefined;
-    expect(strategies!.length).to.be.greaterThan(0);
+    expect(strategies!.length).to.be.greaterThan(1);
   });
 });
 
 
-async function fetchPSPStrategies(config: typeof CONFIG): Promise<ContractInfoPSP[] | undefined> {
-  try {
-    const appConfigResponse = await getConfigFromAppConfig(config);
-    return appConfigResponse;
-  } catch (error) {
-    console.error('Failed to fetch data from app config:', error);
-    return undefined;
-  }
-}
-
-async function getConfigFromAppConfig(config: typeof CONFIG): Promise<ContractInfoPSP[] | undefined> {
-  const client = new AppConfig({region: config.region});
-
-  try {
-    const response = await client.getConfiguration({
-      Application: config.appName,
-      Configuration: config.configName,
-      Environment: config.environmentName,
-      ClientId: config.clientId,
-    });
-
-    if (response.Content) {
-      const configString = Buffer.from(response.Content).toString('utf-8');
-      return JSON.parse(configString) as ContractInfoPSP[];
-    }
-  } catch (error) {
-    throw new Error(`Error fetching configuration: ${error}`);
-  }
-}
