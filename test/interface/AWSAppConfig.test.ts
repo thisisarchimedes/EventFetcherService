639628024c725109e2af06b0efcf4a3d2a12c372
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-16 15:08:02 -0500

    Update AWSAppConfig.test.ts

--- test/interface/AWSAppConfig.test.ts
+++ test/interface/AWSAppConfig.test.ts
@@ -1,22 +1,32 @@
 import {expect} from 'chai';
 import {AppConfig} from '@aws-sdk/client-appconfig';
+import {ContractInfoPSP} from '../../src/types/ContractInfoPSP';
 
-describe('Config Service Test', function() {
+describe('Config Service Demo Environment', function() {
+  const appName: string = 'DemoEnvironment';
+  const configName: string = 'PSPStrategyInfo';
+  const environmentName: string = 'Demo';
+  const clientId:string = '1';
+
   beforeEach(async function() {
   });
 
 
   it('should get PSP strategies via AWS AppConfig', async function() {
-    const res = await getConfig('EventFetcherService-Demo', 'PSPStrategyInfo', 'Demo', '1');
-    console.log(res);
+    const res = await getConfig(appName, configName, environmentName, clientId) as ContractInfoPSP[];
+    expect(res).to.not.be.undefined;
+    expect(res.length).to.be.greaterThan(0);
   });
 
-  async function getConfig(appName: string, configName: string, environmentName: string, clientId: string): Promise<AppConfigResponse | undefined> {
-    // Create an AppConfig client
+  async function getConfig(
+      appName: string,
+      configName: string,
+      environmentName: string,
+      clientId: string,
+  ): Promise<AppConfigResponse | undefined> {
     const client = new AppConfig({region: 'us-east-1'});
 
     try {
-      // Send the command to get the configuration
       const response = await client.getConfiguration({
         Application: appName,
         Configuration: configName,
@@ -25,13 +35,12 @@ describe('Config Service Test', function() {
       });
 
       if (response.Content) {
-        // Assuming the content is in UTF-8 format
         const configString = Buffer.from(response.Content).toString('utf-8');
         return JSON.parse(configString) as AppConfigResponse;
       }
     } catch (error) {
       console.error('Error fetching configuration:', error);
-      throw error; // Or handle it as needed
+      throw error;
     }
   }
 });
