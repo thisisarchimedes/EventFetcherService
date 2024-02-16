e3322060fca3562b5b930b1121c1fcad51c17b0a
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-16 13:39:54 -0500

    update

--- test/interface/ConfigServiceAWSInterface.test.ts
+++ test/interface/ConfigServiceAWSInterface.test.ts
@@ -1,17 +1,17 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
 
-import {ContractInfoLeverage as ContractInfoLeverage} from '../../src/types/ContractInfoLeverage';
-import {ConfigServiceLocal} from '../../src/services/config/ConfigServiceLocal';
-import {ContractInfoPSP} from '../../src/types/ContractInfoPSP';
+import { ContractInfoLeverage as ContractInfoLeverage } from '../../src/types/ContractInfoLeverage';
+import { ConfigServiceLocal } from '../../src/services/config/ConfigServiceLocal';
+import { ContractInfoPSP } from '../../src/types/ContractInfoPSP';
 
 dotenv.config();
 
-describe('Config Service Test', function() {
+describe('Config Service Test', function () {
   let configService: ConfigServiceLocal;
 
-  beforeEach(async function() {
+  beforeEach(async function () {
     await initalizeObjectUnderTest();
   });
 
@@ -24,42 +24,42 @@ describe('Config Service Test', function() {
     await configService.refreshConfig();
   }
 
-  it('should get the correct leverage contract addresses when run locally', async function() {
+  it('should get the correct leverage contract addresses when run locally', async function () {
     const ExpectedLeverageContractAddresses: ContractInfoLeverage[] = await JSON.parse(await fetchStringFromS3(
-        process.env.S3_BUCKET_CONFIG ?? '',
-        process.env.S3_DEPLOYMENT_ADDRESS_KEY ?? '',
+      process.env.S3_BUCKET_CONFIG ?? '',
+      process.env.S3_DEPLOYMENT_ADDRESS_KEY ?? '',
     ));
 
     validateLeverageContractAddress(
-        ExpectedLeverageContractAddresses,
-        'PositionOpener',
-        configService.getLeveragePositionOpenerAddress(),
+      ExpectedLeverageContractAddresses,
+      'PositionOpener',
+      configService.getLeveragePositionOpenerAddress(),
     );
     validateLeverageContractAddress(
-        ExpectedLeverageContractAddresses,
-        'PositionLiquidator',
-        configService.getLeveragePositionLiquidatorAddress(),
+      ExpectedLeverageContractAddresses,
+      'PositionLiquidator',
+      configService.getLeveragePositionLiquidatorAddress(),
     );
     validateLeverageContractAddress(
-        ExpectedLeverageContractAddresses,
-        'PositionCloser',
-        configService.getLeveragePositionCloserAddress(),
+      ExpectedLeverageContractAddresses,
+      'PositionCloser',
+      configService.getLeveragePositionCloserAddress(),
     );
   });
 
   function validateLeverageContractAddress(
-      addresses: ContractInfoLeverage[],
-      contractName: string,
-      actualAddress: string,
+    addresses: ContractInfoLeverage[],
+    contractName: string,
+    actualAddress: string,
   ) {
     const expectedAddress = addresses.find((contract) => contract.name === contractName)?.address;
     expect(actualAddress).to.equal(expectedAddress);
   }
 
-  it('should get the correct PSP contract addresses when run locally', async function() {
+  it('should get the correct PSP contract addresses when run locally', async function () {
     const ExpectedPSPContractInfo: ContractInfoPSP[] = await JSON.parse(await fetchStringFromS3(
-        process.env.S3_BUCKET_CONFIG ?? '',
-        process.env.PSP_STRATEGY_CONFIG_FILE ?? '',
+      process.env.S3_BUCKET_CONFIG ?? '',
+      process.env.PSP_STRATEGY_CONFIG_FILE ?? '',
     ));
     expect(ExpectedPSPContractInfo.length).to.be.greaterThan(0);
 
@@ -70,16 +70,21 @@ describe('Config Service Test', function() {
   });
 
   function getExpectedPSPContractAddress(
-      addresses: ContractInfoPSP[],
-      strategyName: string,
+    addresses: ContractInfoPSP[],
+    strategyName: string,
   ): string {
     return (addresses.find((contract) => contract.strategyName === strategyName)?.strategyAddress) as string;
   }
 
+  it('should retrieve last block scanned', function() {
+    const lastBlockScanned: number = configService.getLastBlockScanned();
+    expect(lastBlockScanned).to.be.greaterThan(19242000);
+  });
+
   async function fetchStringFromS3(bucket: string, key: string): Promise<string> {
     const awsS3Client = createAwsS3Client();
 
-    const params = {Bucket: bucket, Key: key};
+    const params = { Bucket: bucket, Key: key };
     const data = await awsS3Client.getObject(params).promise();
     const configData = data.Body?.toString() ?? '';
     return configData;
