e312a5e2d807b9434388300f58e65078382e033c
danielj86 <danieljafe@gmail.com>
2024-02-19 17:34:46 +0200

    Update EventProcessorService.ts

--- src/EventProcessorService.ts
+++ src/EventProcessorService.ts
@@ -1,16 +1,16 @@
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 import rawEventDescriptors from './events.json';
-import {S3Service, SQSService, Logger} from '@thisisarchimedes/backend-sdk';
+import { S3Service, SQSService, Logger } from '@thisisarchimedes/backend-sdk';
 import dotenv from 'dotenv';
-import {IEventProcessorService} from './IEventProcessorService';
+import { IEventProcessorService } from './IEventProcessorService';
 import {
   ContractType,
   DecodedData,
   EventDescriptor,
 } from './types/EventDescriptor';
-import {EnvironmentContext} from './types/EnvironmentContext';
-import {ProcessedEvent} from './types/ProcessedEvent';
-import {EventData} from './types/EventData';
+import { EnvironmentContext } from './types/EnvironmentContext';
+import { ProcessedEvent } from './types/ProcessedEvent';
+import { EventData } from './types/EventData';
 
 dotenv.config();
 
@@ -18,7 +18,7 @@ const EVENT_DESCRIPTORS: EventDescriptor[] = rawEventDescriptors.map((event) => 
   const obj: EventDescriptor = {
     ...event,
     signature: ethers.utils.id(
-        `${event.name}(${event.decodeData.map((param) => param.type).join(',')})`,
+      `${event.name}(${event.decodeData.map((param) => param.type).join(',')})`,
     ),
     contractType: event.contractType,
   };
@@ -36,12 +36,12 @@ export class EventProcessorService implements IEventProcessorService {
   private readonly _context: EnvironmentContext;
 
   constructor(
-      mainProvider: ethers.providers.Provider,
-      altProvider: ethers.providers.Provider,
-      s3Service: S3Service,
-      sqsService: SQSService,
-      logger: Logger,
-      context: EnvironmentContext,
+    mainProvider: ethers.providers.Provider,
+    altProvider: ethers.providers.Provider,
+    s3Service: S3Service,
+    sqsService: SQSService,
+    logger: Logger,
+    context: EnvironmentContext,
   ) {
     this.mainProvider = mainProvider;
     this.altProvider = altProvider;
@@ -69,12 +69,12 @@ export class EventProcessorService implements IEventProcessorService {
 
       if (events.length > 0) {
         this.logger.info(
-            `Fetched ${events.length} events from blocks ${lastBlock} to ${currentBlock}`,
+          `Fetched ${events.length} events from blocks ${lastBlock} to ${currentBlock}`,
         );
         await this.queueEvents(events);
       } else {
         this.logger.info(
-            `No new events found on blocks ${lastBlock} to ${currentBlock}`,
+          `No new events found on blocks ${lastBlock} to ${currentBlock}`,
         );
       }
       this.logger.info('4');
@@ -95,7 +95,7 @@ export class EventProcessorService implements IEventProcessorService {
   }
 
   private async fetchLogsFromProviders(
-      filter: ethers.providers.Filter,
+    filter: ethers.providers.Filter,
   ): Promise<ethers.providers.Log[]> {
     const [alchemyLogs, infuraLogs] = await Promise.all([
       this.mainProvider.getLogs(filter),
@@ -119,105 +119,103 @@ export class EventProcessorService implements IEventProcessorService {
   }
 
   public decodeAndProcessLogs(
-      logs: ethers.providers.Log[],
-      descriptor: EventDescriptor,
+    logs: ethers.providers.Log[],
+    descriptor: EventDescriptor,
   ): ProcessedEvent[] {
     const logRes = logs
-        .map((log) => {
-          const indexedTypes = descriptor.decodeData
-              .filter((param: DecodedData) => param.indexed)
-              .map((param: DecodedData) => param.type);
+      .map((log) => {
+        const indexedTypes = descriptor.decodeData
+          .filter((param: DecodedData) => param.indexed)
+          .map((param: DecodedData) => param.type);
 
-          const nonIndexedTypes = descriptor.decodeData
-              .filter((param: DecodedData) => !param.indexed)
-              .map((param: DecodedData) => param.type);
+        const nonIndexedTypes = descriptor.decodeData
+          .filter((param: DecodedData) => !param.indexed)
+          .map((param: DecodedData) => param.type);
 
-          try {
+        try {
           // Decode non-indexed parameters from: log.data
-            const nonIndexedData = ethers.utils.defaultAbiCoder.decode(
-                nonIndexedTypes,
-                log.data,
-            );
+          const nonIndexedData = ethers.utils.defaultAbiCoder.decode(
+            nonIndexedTypes,
+            log.data,
+          );
 
-            // Decode indexed parameters from: log.topics
-            const indexedData: ethers.utils.Result[] = indexedTypes.map(
-                (type: string, index: number) => {
-                  const topic = log.topics[index + 1];
-                  return ethers.utils.defaultAbiCoder.decode([type], topic)[0];
-                },
-            );
+          // Decode indexed parameters from: log.topics
+          const indexedData: ethers.utils.Result[] = indexedTypes.map(
+            (type: string, index: number) => {
+              const topic = log.topics[index + 1];
+              return ethers.utils.defaultAbiCoder.decode([type], topic)[0];
+            },
+          );
 
-            // Merge both indexed and non-indexed data
-            const allData: ethers.utils.Result[] = [
-              ...indexedData,
-              ...nonIndexedData,
-            ];
+          // Merge both indexed and non-indexed data
+          const allData: ethers.utils.Result[] = [
+            ...indexedData,
+            ...nonIndexedData,
+          ];
 
-            const eventData: EventData = {};
-            descriptor.decodeData.forEach((param: DecodedData, index: number) => {
-              eventData[param.name] = allData[index].toString();
-            });
+          const eventData: EventData = {};
+          descriptor.decodeData.forEach((param: DecodedData, index: number) => {
+            eventData[param.name] = allData[index].toString();
+          });
 
-            const retObj: ProcessedEvent = {
-              name: descriptor.name,
-              contractType: descriptor.contractType,
-              txHash: log.transactionHash,
-              blockNumber: log.blockNumber,
-              data: eventData,
-            };
+          const retObj: ProcessedEvent = {
+            name: descriptor.name,
+            contractType: descriptor.contractType,
+            txHash: log.transactionHash,
+            blockNumber: log.blockNumber,
+            data: eventData,
+          };
 
-            return retObj;
-          } catch (error) {
-            this.logger.error(`Failed to decode log: ${JSON.stringify(error)}`);
-            return null;
-          }
-        })
-        .filter((event): event is ProcessedEvent => event !== null);
+          return retObj;
+        } catch (error) {
+          this.logger.error(`Failed to decode log: ${JSON.stringify(error)}`);
+          return null;
+        }
+      })
+      .filter((event): event is ProcessedEvent => event !== null);
 
     return logRes;
   }
 
-  private async fetchAndProcessEvents(
-      fromBlock: number,
-      toBlock: number,
-  ): Promise<ProcessedEvent[]> {
+  private getContractAddressByType(contractType: ContractType): string {
+    switch (contractType) {
+      case ContractType.Opener:
+        return this._context.positionOpenerAddress;
+      case ContractType.Closer:
+        return this._context.positionCloserAddress;
+      case ContractType.Liquidator:
+        return this._context.positionLiquidatorAddress;
+      case ContractType.Expirator:
+        return this._context.positionExpiratorAddress;
+      default:
+        throw new Error(`Unknown contract type: ${contractType}`);
+    }
+  }
+
+
+  private async fetchAndProcessEvents(fromBlock: number, toBlock: number): Promise<ProcessedEvent[]> {
     const processedEvents: ProcessedEvent[] = [];
-    for (
-      let startBlock = fromBlock + 1;
-      startBlock <= toBlock;
-      startBlock += this._context.EVENTS_FETCH_PAGE_SIZE
-    ) {
-      const endBlock = Math.min(
-          startBlock + this._context.EVENTS_FETCH_PAGE_SIZE - 1,
-          toBlock,
-      );
-      for (const descriptor of this.EVENT_DESCRIPTORS) {
-        let filter = {};
 
-        const contractAddress =
-          descriptor.contractType == ContractType.Opener ?
-            this._context.positionOpenerAddress :
-            descriptor.contractType == ContractType.Closer ?
-              this._context.positionCloserAddress :
-              descriptor.contractType == ContractType.Liquidator ?
-                this._context.positionLiquidatorAddress :
-                descriptor.contractType == ContractType.Expirator ?
-                  this._context.positionExpiratorAddress :
-                  '';
+    for (let startBlock = fromBlock + 1; startBlock <= toBlock; startBlock += this._context.EVENTS_FETCH_PAGE_SIZE) {
+      const endBlock = Math.min(startBlock + this._context.EVENTS_FETCH_PAGE_SIZE - 1, toBlock);
 
-        if (contractAddress.length == 0) continue;
-
-        filter = {
-          address: contractAddress,
+      // Create a combined filter for all events
+      let filters = this.EVENT_DESCRIPTORS.map(descriptor => {
+        return {
+          address: this.getContractAddressByType(descriptor.contractType),
           topics: [descriptor.signature],
           fromBlock: startBlock,
-          toBlock: endBlock + 5,
+          toBlock: endBlock
         };
+      });
 
-        const fetchedLogs = await this.fetchLogsFromProviders(filter);
-        const uniqueLogs = this.deduplicateLogs(fetchedLogs);
-        const processedLogs = this.decodeAndProcessLogs(uniqueLogs, descriptor);
+      // Fetch logs once for each block range
+      const fetchedLogs = await this.fetchLogsFromCombinedFilters(filters);
 
+      // Process fetched logs for each event descriptor
+      for (const descriptor of this.EVENT_DESCRIPTORS) {
+        const relevantLogs = fetchedLogs.filter(log => log.topics.includes(descriptor.signature));
+        const processedLogs = this.decodeAndProcessLogs(relevantLogs, descriptor);
         processedEvents.push(...processedLogs);
       }
     }
@@ -225,15 +223,24 @@ export class EventProcessorService implements IEventProcessorService {
     return processedEvents;
   }
 
+  private async fetchLogsFromCombinedFilters(filters: ethers.providers.Filter[]): Promise<ethers.providers.Log[]> {
+    const allLogs = [];
+    for (const filter of filters) {
+      const logs = await this.fetchLogsFromProviders(filter);
+      allLogs.push(...logs);
+    }
+    return this.deduplicateLogs(allLogs);
+  }
+
   private async queueEvents(events: ProcessedEvent[]): Promise<void> {
     for (const event of events) {
       this.logger.info(
-          `Appending message to queue ${this._context.NEW_EVENTS_QUEUE_URL
-          }\n msg ${JSON.stringify(event)}`,
+        `Appending message to queue ${this._context.NEW_EVENTS_QUEUE_URL
+        }\n msg ${JSON.stringify(event)}`,
       );
       await this.sqsService.sendMessage(
-          this._context.NEW_EVENTS_QUEUE_URL,
-          JSON.stringify(event),
+        this._context.NEW_EVENTS_QUEUE_URL,
+        JSON.stringify(event),
       );
     }
   }
@@ -250,9 +257,9 @@ export class EventProcessorService implements IEventProcessorService {
     const bucket = this._context.S3_BUCKET;
 
     await this.s3Service.putObject(
-        bucket,
-        this._context.S3_LAST_BLOCK_KEY,
-        blockNumber.toString(),
+      bucket,
+      this._context.S3_LAST_BLOCK_KEY,
+      blockNumber.toString(),
     );
   }
 }
