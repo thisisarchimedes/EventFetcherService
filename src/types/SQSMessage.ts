924ba0b26bb2963ed8de5999d5efdc97be8065ed
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-21 17:00:10 -0500

    Update SQSMessage.ts

--- src/types/SQSMessage.ts
+++ src/types/SQSMessage.ts
@@ -1,6 +1,6 @@
 import { ContractType } from './EventDescriptor';
 
-export type SQSMessage = {
+export type EventFetcherSQSMessage = {
   name: string;
   contractType: ContractType;
   txHash: string;
