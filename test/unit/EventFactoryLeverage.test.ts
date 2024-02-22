99bfed07b09e6a65bca886959df99ac04fe6d6f2
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-21 21:33:48 -0500

    Update EventFactoryLeverage.test.ts

--- test/unit/EventFactoryLeverage.test.ts
+++ test/unit/EventFactoryLeverage.test.ts
@@ -10,6 +10,7 @@ import {EventFetcherLogEntryMessage} from '../../src/types/NewRelicLogEntry';
 import {SQSServiceAdapter} from '../adapters/SQSServiceAdapter';
 import {OnChainEvent} from '../../src/onchain_events/OnChainEvent';
 import {EventFetcherSQSMessage} from '../../src/types/SQSMessage';
+import exp from 'constants';
 
 describe('Leverage Events Logging & Queuing', function() {
   let logger: LoggerAdapter;
@@ -32,7 +33,42 @@ describe('Leverage Events Logging & Queuing', function() {
   });
 
   it('should report on PositionOpened event', async function() {
-    eventFetcher.setEventArrayFromFile('test/data/leveragePositionOpenedEvent.json');
+    const eventDataFileName: string = 'test/data/leveragePositionOpenedEvent.json';
+    const expectedEventName: string = 'LeveragedPositionOpened';
+
+    const expectedLogMessage: EventFetcherLogEntryMessage = {
+      event: 'LeveragedPositionOpened',
+      user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
+      strategy: 'Convex FRAXBP/msUSD Single Pool',
+      depositAmount: '1',
+      borrowedAmount: '2',
+    };
+
+    const expectedSqsMessage: EventFetcherSQSMessage = {
+      name: 'PositionOpened',
+      contractType: 0,
+      txHash: '0x1fe52317d52b452120708667eed57e3c19ad39268bfabcf60230978c50df426f',
+      blockNumber: 6000003,
+      data: {
+        nftId: 2,
+        user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
+        strategy: '0x825cc02ec7B77d4432e82e7bCAf3B89a67a555F1',
+        collateralAmount: '1',
+        wbtcToBorrow: '2',
+        positionExpireBlock: '3',
+        sharesReceived: '4',
+      },
+    };
+
+    const event: OnChainEvent = await testEventGeneration(eventDataFileName, expectedEventName);
+    testEventProcessing(event, expectedLogMessage, expectedSqsMessage);
+  });
+
+  async function testEventGeneration(
+      eventFileName: string,
+      expetedEventName: string,
+  ): Promise<OnChainEvent> {
+    eventFetcher.setEventArrayFromFile(eventFileName);
     const eventsLog = await eventFetcher.getOnChainEvents(100, 200);
 
     const onChainEvents: OnChainEvent[] = [];
@@ -50,38 +86,24 @@ describe('Leverage Events Logging & Queuing', function() {
     expect(onChainEvents.length).to.equal(1);
 
     const event: OnChainEvent = onChainEvents[0];
-    expect(event.getEventName()).to.equal('LeveragedPositionOpened');
+    expect(event.getEventName()).to.equal(expetedEventName);
 
+    return event;
+  }
+
+  function testEventProcessing(
+      event: OnChainEvent,
+      expectedLogMessage: EventFetcherLogEntryMessage,
+      expectedSqsMessage: EventFetcherSQSMessage,
+  ) {
     event.process();
 
-    const expectedLogMessage: EventFetcherLogEntryMessage = {
-      event: 'LeveragedPositionOpened',
-      user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
-      strategy: 'Convex FRAXBP/msUSD Single Pool',
-      depositAmount: '1',
-      borrowedAmount: '2',
-    };
     const actualLogMessage = JSON.parse(JSON.parse(logger.getLastMessageRawString().split('INFO: ')[1]));
     validateLogMessage(actualLogMessage, expectedLogMessage);
 
-    const expectedSqsMessage: EventFetcherSQSMessage = {
-      name: 'PositionOpened',
-      contractType: 0,
-      txHash: '0x1fe52317d52b452120708667eed57e3c19ad39268bfabcf60230978c50df426f',
-      blockNumber: 6000003,
-      data: {
-        nftId: 2,
-        user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
-        strategy: '0x825cc02ec7B77d4432e82e7bCAf3B89a67a555F1',
-        collateralAmount: '1',
-        wbtcToBorrow: '2',
-        positionExpireBlock: '3',
-        sharesReceived: '4',
-      },
-    };
     const actualSqsMessage = JSON.parse(sqsService.getLatestMessage());
     validateSQSMessage(actualSqsMessage, expectedSqsMessage);
-  });
+  }
 
   it('should report on PositionClosed event', async function() {
     eventFetcher.setEventArrayFromFile('test/data/leveragePositionClosedEvent.json');
