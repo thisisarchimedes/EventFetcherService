3d5edc883db81db77d9a1dc38acdad38017b5671
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-22 09:54:28 -0500

    Update EventFactoryLeverage.test.ts

--- test/unit/EventFactoryLeverage.test.ts
+++ test/unit/EventFactoryLeverage.test.ts
@@ -33,7 +33,7 @@ describe('Leverage Events Logging & Queuing', function() {
   });
 
   it('should report on PositionOpened event', async function() {
-    const eventDataFileName: string = 'test/data/leveragePositionOpenedEvent.json';
+    const eventSyntheticDataFileName: string = 'test/data/leveragePositionOpenedEvent.json';
     const expectedEventName: string = 'LeveragedPositionOpened';
 
     const expectedLogMessage: EventFetcherLogEntryMessage = {
@@ -60,9 +60,42 @@ describe('Leverage Events Logging & Queuing', function() {
       },
     };
 
-    const event: OnChainEvent = await testEventGeneration(eventDataFileName, expectedEventName);
+    const event: OnChainEvent = await testEventGeneration(eventSyntheticDataFileName, expectedEventName);
     testEventProcessing(event, expectedLogMessage, expectedSqsMessage);
   });
+
+  it('should report on PositionClosed event', async function() {
+    const eventSyntheticDataFileName: string = 'test/data/leveragePositionClosedEvent.json';
+    const expectedEventName: string = 'LeveragedPositionClosed';
+
+    const expectedLogMessage: EventFetcherLogEntryMessage = {
+      event: 'LeveragedPositionClosed',
+      user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
+      strategy: 'Convex FRAXBP/msUSD Single Pool',
+      depositAmount: '1',
+      borrowedAmount: '2',
+    };
+
+    const expectedSqsMessage: EventFetcherSQSMessage = {
+      name: 'PositionClosed',
+      contractType: 1,
+      txHash: '0x1fe52317d52b452120708667eed57e3c19ad39268bfabcf60230978c50df426f',
+      blockNumber: 6000003,
+      data: {
+        nftId: 2,
+        user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
+        strategy: '0x825cc02ec7B77d4432e82e7bCAf3B89a67a555F1',
+        collateralAmount: '1',
+        wbtcToBorrow: '2',
+        positionExpireBlock: '',
+        sharesReceived: '',
+      },
+    };
+
+    const event: OnChainEvent = await testEventGeneration(eventSyntheticDataFileName, expectedEventName);
+    testEventProcessing(event, expectedLogMessage, expectedSqsMessage);
+  });
+
 
   async function testEventGeneration(
       eventFileName: string,
@@ -104,59 +137,6 @@ describe('Leverage Events Logging & Queuing', function() {
     const actualSqsMessage = JSON.parse(sqsService.getLatestMessage());
     validateSQSMessage(actualSqsMessage, expectedSqsMessage);
   }
-
-  it('should report on PositionClosed event', async function() {
-    eventFetcher.setEventArrayFromFile('test/data/leveragePositionClosedEvent.json');
-    const eventsLog = await eventFetcher.getOnChainEvents(100, 200);
-
-    const onChainEvents: OnChainEvent[] = [];
-    for (const event of eventsLog) {
-      try {
-        const evt = eventFactory.createEvent(event);
-        onChainEvents.push(evt);
-      } catch (e) {
-        if (e.message === 'Unknown contract address') {
-          continue;
-        }
-      }
-    }
-
-    expect(onChainEvents.length).to.equal(1);
-
-    const event: OnChainEvent = onChainEvents[0];
-    expect(event.getEventName()).to.equal('LeveragedPositionClosed');
-
-    event.process();
-
-    const expectedLogMessage: EventFetcherLogEntryMessage = {
-      event: 'LeveragedPositionClosed',
-      user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
-      strategy: 'Convex FRAXBP/msUSD Single Pool',
-      depositAmount: '1',
-      borrowedAmount: '2',
-    };
-    const actualLogMessage = JSON.parse(JSON.parse(logger.getLastMessageRawString().split('INFO: ')[1]));
-    validateLogMessage(actualLogMessage, expectedLogMessage);
-
-    const expectedSqsMessage: EventFetcherSQSMessage = {
-      name: 'PositionClosed',
-      contractType: 1,
-      txHash: '0x1fe52317d52b452120708667eed57e3c19ad39268bfabcf60230978c50df426f',
-      blockNumber: 6000003,
-      data: {
-        nftId: 2,
-        user: '0x925cc02EC7b77d4432e82e7bCaf3B89a67a555F2',
-        strategy: '0x825cc02ec7B77d4432e82e7bCAf3B89a67a555F1',
-        collateralAmount: '1',
-        wbtcToBorrow: '2',
-        positionExpireBlock: '',
-        sharesReceived: '',
-      },
-    };
-    const actualSqsMessage = JSON.parse(sqsService.getLatestMessage());
-    validateSQSMessage(actualSqsMessage, expectedSqsMessage);
-  });
-
 
   function validateLogMessage(
       actualLogMessage: EventFetcherLogEntryMessage,
