60a627a45343ebff0f472e9486d052061c91f690
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-26 21:24:01 -0500

    Update RDSDB.test.ts

--- test/interface/RDSDB.test.ts
+++ test/interface/RDSDB.test.ts
@@ -18,4 +18,35 @@ describe('RDS DB with Prisma', function() {
     expect(openPosition).to.not.be.null;
     expect(openPosition?.nftId).to.equal(BigInt(1));
   });
+
+  it('should write to RDS DB', async function() {
+    await prisma.openLeverage.create({
+      data: {
+        txHash: '0x123',
+        blockNumber: 600000,
+        nftId: BigInt(139489340),
+        user: '0x123',
+        strategy: '0x123',
+        collateralAmount: Number(1),
+        wbtcToBorrow: Number(1),
+        positionExpireBlock: Number(600000),
+        receivedShares: Number(1),
+      },
+    });
+
+    const openPosition = await prisma.openLeverage.findFirst({
+      where: {
+        nftId: BigInt(139489340),
+      },
+    });
+
+    expect(openPosition).to.not.be.null;
+    expect(openPosition?.nftId).to.equal(BigInt(139489340));
+
+    await prisma.openLeverage.delete({
+      where: {
+        id: openPosition?.id,
+      },
+    });
+  });
 });
